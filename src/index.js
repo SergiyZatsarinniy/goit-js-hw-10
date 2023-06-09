4fba5a1e1ca3de70959bdf0c9cc99290d50654ea
SergiyZatsarinniy <Zatsarinniys@gmail.com>
2023-06-09 12:39:27 +0300

    summary

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api.js';
+import './css/style.css';
+
+const API_KEY =
+  'live_qE21O52c52A37ctUcV6hliaEyBnz59hGiAFTOKXxN2VjhrYLa3zzbYTrw7v0eoiT';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+const refs = {
+  select: document.querySelector('.breed-select'),
+  animalCard: document.querySelector('.cat-info'),
+  alertLoader: document.querySelector('.loader'),
+  alertError: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
+
+refs.select.addEventListener('change', onNewChoiceAnimal);
+refs.alertLoader.classList.add('is-hidden');
+
+function onNewChoiceAnimal(event) {
+  const breed_ids = event.target.value;
+  return fetchCatByBreed(breed_ids);
+}
+
+fetchBreeds(`${BASE_URL}/breeds?${API_KEY}`);
+
+export { refs, API_KEY, BASE_URL };
---
+++ src/js/cat-api.js
@@ -0,0 +1,121 @@
+import { refs, API_KEY, BASE_URL } from '../index.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+// function fetchBreeds() {
+//   showLoader();
+
+//   return fetch('https://api.thecatapi.com/v1/breeds')
+//     .then(response => response.json())
+//     .then(data => {
+//       return data.map(breed => ({
+//         id: breed.id,
+//         name: breed.name,
+//       }));
+//     })
+//     .catch(onShowError());
+// }
+
+// function fetchCats() {}
+
+// function fetchCatByBreed(breedId) {
+//   fetch(`${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
+//     .then((response) => {
+
+//       return;
+//     }
+//       }
+
+// function onShowError() {
+//   Notify.failure('Error loading page...');
+// }
+
+Notify.init({
+  width: '500px',
+  fontSize: '25px',
+  position: 'center-top',
+  timeout: '5000',
+  messageMaxLength: 150,
+  distance: '20px',
+  showOnlyTheLastOne: true,
+  warning: {
+    background: 'rgba(190, 194, 79, 1)',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-warning',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-exclamation-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,1)',
+    backOverlayColor: 'rgba(238,191,49,0.2)',
+  },
+});
+
+function fetchBreeds(adress) {
+  onShowLoadListAction();
+  return fetchAnimals(adress)
+    .then(response => {
+      // fetchCatByBreed(response[0].id);
+      renderSelect(response);
+      onShowLoadListAction();
+      return;
+    })
+    .catch(() => onShowError());
+}
+
+function fetchAnimals(adress) {
+  return fetch(adress)
+    .then(response => {
+      return response.json();
+    })
+    .catch(() => onShowError());
+}
+
+function renderSelect(animals) {
+  const markup = animals
+    .map(elem => {
+      return `<option value="${elem.id}">${elem.name}</option>`;
+    })
+    .join('');
+  refs.select.insertAdjacentHTML('afterbegin', markup);
+  new SlimSelect({
+    select: '#first-select',
+  });
+  return;
+}
+
+function fetchCatByBreed(breedId) {
+  onShowLoadAnimalAction();
+  return fetchAnimals(
+    `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  )
+    .then(response => {
+      renderAnimalCard(...response);
+      onShowLoadAnimalAction();
+      return;
+    })
+    .catch(() => onShowError());
+}
+
+function renderAnimalCard(animal) {
+  const markup = `<img src="${animal.url}" alt='cat' width=700><div class="cat-description">
+    <p class='cat-titles'> ${animal.breeds[0].name}</p>
+    <p> ${animal.breeds[0].description} </p>
+    <p><span class='cat-temp'>Temperament:</span> ${animal.breeds[0].temperament}</p></div>`;
+  return (refs.animalCard.innerHTML = markup);
+}
+
+function onShowLoadListAction() {
+  refs.alertLoader.classList.toggle('is-hidden');
+  refs.select.classList.toggle('is-hidden');
+}
+
+function onShowLoadAnimalAction() {
+  refs.alertLoader.classList.toggle('is-hidden');
+  refs.catInfo.classList.toggle('is-hidden');
+}
+
+function onShowError() {
+  Notify.failure('Error loading page...');
+}
+
+export { fetchBreeds, fetchCatByBreed };
